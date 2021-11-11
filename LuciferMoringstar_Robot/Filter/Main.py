e9ce2833284de8b17dda465ed106bef794bfc6a0
SriGuru05 <94064094+SriGuru05@users.noreply.github.com>
2021-11-11 09:10:43 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -277,7 +277,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "help":
             buttons = [
                 [
-                    InlineKeyboardButton('🥇 Join My Channel 🥇', url=f'{TUTORIAL}')
+                    InlineKeyboardButton('🎖 Join My Channel 🎖', url=f'{TUTORIAL}')
                 ]
                 ]
             await query.message.edit(text=f"{HELP}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
@@ -285,7 +285,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('🥇 Join My Channel 🥇', url=f'{TUTORIAL}')
+                    InlineKeyboardButton('🎖 Join My Channel 🎖', url=f'{TUTORIAL}')
                 ]
                 ]
             await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
@@ -308,7 +308,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🥇 Join My Channel 🥇', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🎖 Join My Channel 🎖', url=f'{TUTORIAL}')
                     ]
                     ]
                 
@@ -339,7 +339,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🥇 Join My Channel 🥇', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🎖 Join My Channel 🎖', url=f'{TUTORIAL}')
                     ]
                     ]
                 
